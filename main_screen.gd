9e3691828f40bfaff3376e195b6c4619c0bb11a0
Eugene-Tshien <100738608+Eugene-Tshien@users.noreply.github.com>
2024-10-13 17:12:20 +1100

    shader

--- main_screen.gd
+++ main_screen.gd
@@ -27,8 +27,17 @@ func _input(event):
 
 
 func _on_button_pressed() -> void:
+	var g = $Game
+	g.use_parent_material = true;
 	myBool = true
 	unmyBool = false
+	
+	
+func _on_sliding_back_button_pressed():
+	var g = $Game
+	g.use_parent_material = false;
+	unmyBool = true
+	myBool = false
 	
 func moveCam() -> void:
 	var button = get_node("Button")
@@ -41,7 +50,7 @@ func moveCam() -> void:
 	camera.zoom = camera.zoom.lerp(zoomed_out, 0.05)
 		
 	var slideButton = $"Sliding back button"
-	slideButton.position = slideButton.position.lerp(Vector2(67, 85), 0.05)
+	slideButton.position = slideButton.position.lerp(Vector2(67, 85), 0.18)
 	
 func unmoveCam() -> void:
 	var camera = $Camera2D
@@ -49,9 +58,4 @@ func unmoveCam() -> void:
 	camera.zoom = camera.zoom.lerp(zoomed_in, 0.05)
 	
 	var slideButton = $"Sliding back button"
-	slideButton.position = slideButton.position.lerp(Vector2(-400, 85), 0.05)
-
-
-func _on_sliding_back_button_pressed():
-	unmyBool = true
-	myBool = false
+	slideButton.position = slideButton.position.lerp(Vector2(-400, 85), 0.18)
