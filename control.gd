ce14d59e9ae6411a29eefb2519f4a7f6d1c271a6
Kyluuuu <xthegrimcreeper@gmail.com>
2024-10-15 18:56:17 +1100

    Integrated git

--- control.gd
+++ control.gd
@@ -1,10 +1,11 @@
 extends Control
 
+#0 for nothing, 1 for crosses, 2 for circles
+var tileState
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	pass # Replace with function body.
+	tileState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
