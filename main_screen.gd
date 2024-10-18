d0eb8ab7f00f7016c0c9271a388b040e5b24e1c1
Kyluuuu <xthegrimcreeper@gmail.com>
2024-10-19 00:16:30 +1100

    stuff

--- main_screen.gd
+++ main_screen.gd
@@ -1,7 +1,16 @@
 extends Node2D
 
+# 0 : X, 1 : O
+var playerTurn
+
 func _ready() -> void:
-	pass
+	playerTurn = 0
 
 func _process(delta: float) -> void:
 	pass
+
+func _updatePlayerTurn() -> void:
+	if playerTurn == 0:
+		playerTurn = 1
+	else:
+		playerTurn = 0
